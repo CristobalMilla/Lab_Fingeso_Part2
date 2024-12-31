9c03998f6a07f0440ba4dbaf77981616af95213a
Cristobal Milla <cristobal.milla@usach.cl>
2024-12-30 21:11:58 -0300

    Avance 30/12, pagina userType

--- FrontEnd/AutoAltiro/src/views/userType.vue
+++ FrontEnd/AutoAltiro/src/views/userType.vue
@@ -1,5 +1,38 @@
 <script setup>
-    
+    import axios from 'axios';
+    import {ref, onBeforeMount} from 'vue';
+    let developer = false;
+    let admin = false;
+    const client = ref(false);
+    let employee = false;
+    const perfiles = ref([]);
+    const loading = ref(true);
+    onBeforeMount(async ()=> {
+        try {
+            const idUsuario = await axios.get(import.meta.env.VITE_BASE_URL + "api/usuario/usuarioActual/id");
+            const respuesta = await axios.get(import.meta.env.VITE_BASE_URL + "api/usuario/obtenerPerfilesById/" + idUsuario.data);
+            perfiles.value = respuesta.data;
+            console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            if(perfiles.value.includes('desarrollador')){
+                developer = true;
+            };
+            if(perfiles.value.includes("administrador")){
+                admin = true;
+            };
+            if(perfiles.value.includes('Cliente')){
+                console.log("AAAAAAAAAA");
+                client.value = true;
+                console.log(client);
+            };
+            if(perfiles.value.includes("empleado")){
+                employee = true;
+            };
+        } catch (error) {
+            alert(error);
+        } finally {
+            loading.value=false;
+        }
+    });
 </script>
 
 <template>
@@ -23,12 +56,16 @@
                     <div class="alsoButton" @click="handleChange">Ingresar como cliente</div>
                 </router-link>
             </div>
-            <div class="inputContainers" v-if="employer">
+            <div class="inputContainers" v-if="employee">
                 <router-link to ='/empleado'>
                     <div class="alsoButton" @click="handleChange">Ingresar como empleado</div>
                 </router-link>
             </div>
             <div class="headerDescription" v-else>Su usuario no tiene los permisos correctos</div>
+            <div class="inputContainers">
+                <div class="alsoButton" @click="recuperarIdUsuario">XD</div>
+                <div class="alsoButton" @click="permisos">XDDDDDDDD</div>
+            </div>
         </div>
     </div>
    </main>
@@ -44,48 +81,43 @@
     
 }
     export default{
-        data(){
-            return{
-                developer: false,
-                admin: false,
-                client: false,
-                employer: false,
-            }
-        },
         methods:{
             async recuperarIdUsuario(){
-                const idUsuario = 0,
                 try {
-                    idUsuario = await axios.get(import.meta.env.VITE_BASE_URL + "api/usuario/usuarioActual/id");
+                    const idUsuario = await axios.get(import.meta.env.VITE_BASE_URL + "api/usuario/usuarioActual/id");
+                    console.log(idUsuario);
                 } catch (error) {
-                    alert("No se pudo recuperar el Id del usuario logueado")
+                    alert("No se pudo recuperar el Id del usuario logueado");
                 } 
             },
             async permisos(){
                 //Solcitud de datos backend
                 try {
-                    const respuesta = await axios.get(import.meta.env.VITE_BASE_URL + "api/usuario/obtenerPerfilesById/" + idUsuario);
-                    console.log(respuesta)
+                    const idUsuario = await axios.get(import.meta.env.VITE_BASE_URL + "api/usuario/usuarioActual/id");
+                    const respuesta = await axios.get(import.meta.env.VITE_BASE_URL + "api/usuario/obtenerPerfilesById/" + idUsuario.data);
+                    console.log(respuesta);
+                    /*
+                    if(listaTipos.includes("desarrollador")){
+                        developer = true;
+                    };
+                    if(listaTipos.includes("administrador")){
+                        admin = true;
+                    };
+                    if(listaTipos.includes("cliente")){
+                        client = true;
+                    };
+                    if(listaTipos.includes("empleado")){
+                        employer = true;
+                    };*/ 
+
                 } catch (error) {
-                    alert("No se pudo recuperar la lista de perfiles del usuario logueado")
+                    alert("No se pudo recuperar la lista de perfiles del usuario logueado");
                 }
             },
         }
         
-    }/*
-    if(userTypes.includes("desarrollador")){
-            developer = 1;
     };
-    if(userTypes.includes("administrador")){
-            admin = 1;
-    };
-    if(userTypes.includes("cliente")){
-            client = 1;
-    };
-    if(userTypes.includes("empleado")){
-            employer = 1;
-    }; */
-
+    
 
 </script>
 

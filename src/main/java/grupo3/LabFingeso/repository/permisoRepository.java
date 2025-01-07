42d44fbf3dd55660a52e60df649c41db59c51809
Luc1809 <lucas.contador@usach.cl>
2025-01-07 11:52:22 -0300

    Eliminacion archivos no utilizados

--- src/main/java/grupo3/LabFingeso/repository/permisoRepository.java
+++
@@ -1,8 +0,0 @@
-package grupo3.LabFingeso.repository;
-
-import grupo3.LabFingeso.entity.permisoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface permisoRepository extends JpaRepository<permisoEntity, Long> {
-
-}
